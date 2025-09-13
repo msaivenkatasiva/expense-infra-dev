1c5071bbec155bf7978d29435f674a370cd48fd1
msaivenkatasiva <sivasl0597@gmail.com>
2025-09-13 19:30:25 +0530

    expense infra

--- 07-backend/backend.sh
+++ 07-backend/backend.sh
@@ -3,3 +3,4 @@ component=$1
 environment=$2
 dnf install ansible -y
 pip3.9 install botocore boto3
+ansible-pull -i localhost, -U https://github.com/msaivenkatasiva/expense-ansible-roles-tf.git main.yaml -e component=$component -e env=$environment
